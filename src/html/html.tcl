d3fb09f0d35af2ca0b9aef64a43618afe6b3c699
joseph lenton <jl235@kent.ac.uk>
2013-06-28 06:18:03 +0100

    html tag creation, and parsing of tag names
    Html tags can now be created, and described using CSS-like descriptions
    of the element.

--- src/html/html.tcl
+++ src/html/html.tcl
@@ -3,36 +3,27 @@ package require struct::set
 
 package provide html 1.0
 
-namespace html {
-    namespace export html
+namespace eval ::html {
+    namespace export html htmlStr
     namespace ensemble create
-
-    variable htmlTags
-    variable viewStore
-
-    strict::set include htmlTags div span p article header footer
-    strict::set include htmlTags h1 h2 h3 h4 h5
-    strict::set include htmlTags ul ol li
-    strict::set include htmlTags canvas video audio
-    strict::set include htmlTags table tbody thead th tr td
-    strict::set include htmlTags form input textarea label select option
-    strict::set include htmlTags code pre
-
-    array set viewStore {}
 }
 
-proc html {rules} {
-    set str {}
+# 
+# This builds the HTML, and then returns it as a string.
+#
+proc html::htmlStr args {
+    set tagMeta [ lindex $args 0 ]
+    set content [ lreplace $args 0 0 ]
+    set tag [ getTagType $tagMeta ]
 
-    foreach html $rules {
-        append str [ htmlRule $html ]
-    }
-    
-    return str
+    return "<[ getOpenTag $tagMeta ]>[ join $content {} ]</$tag>"
 }
 
 #
-#    html div {
+# This is the *same* as html::htmlStr, except the result is printed instead of
+# returned. That is the only difference.
+#
+#    [ html div 
 #        [ html h1 my article title ]
 #    
 #        here is some example text
@@ -40,16 +31,103 @@ proc html {rules} {
 #        [ html article 
 #            the body of the article goes here
 #        ]
-#   }
+#    ]
 #
-proc html {id} {
-    variable htmlTags
+proc html::html args {
+    puts [ html::htmlStr {*}$args ]
+}
 
-    if { struct::set contains htmlTags $id } {
-        set tag $id
+proc getTagType tag {
+    # find out the tag type
+    set firstChar [ string index $tag 0 ]
+
+    if { $firstChar == "." || $firstChar == "#" } {
+        return div
     } else {
-        set tag div
+        set nextDot  [ string first "." $tag ]
+        set nextHash [ string first "#" $tag ]
+
+        if { $nextDot == -1 } {
+            if { $nextHash != -1 } {
+                return [ string range $tag 0 [ expr $nextHash -1 ] ]
+            } else {
+                return $tag
+            }
+        } elseif { $nextHash == -1 } {
+            if { $nextDot != -1 } {
+                return [ string range $tag 0 [ expr $nextDot -1 ] ]
+            } else { 
+                return $tag
+            }
+        } else {
+            return [ string range $tag 0 [ expr min($nextDot, $nextHash) -1 ] ]
+        }
+
+        return div
+    }
+}
+
+proc getOpenTag tag {
+    set class {}
+    set id {}
+
+    # chomp along the tag, collecting classes and id's
+    set i 0
+    set lastI 0
+    set nextDot -1
+    set nextHash -1
+    set isClass 0
+    while { $i < [string length $tag] } {
+        set nextDot  [ expr [ string first "." $tag $i ] + 1 ]
+        set nextHash [ expr [ string first "#" $tag $i ] + 1 ]
+
+        if { $nextDot != 0 && ( $nextHash == 0 || $nextDot < $nextHash ) } {
+            set i $nextDot
+            set nextClass 1
+
+        } elseif { $nextHash != 0 } {
+            set i $nextHash
+            set nextClass 0
+
+        # no IDs or Classes in the string
+        } elseif { $lastI == 0 } {
+            break
+
+        # there are IDs and Classes, but we are at the end
+        } else {
+            if { $isClass } {
+                append class [ string range $tag $lastI end ]
+            } else {
+                append id [ string range $tag $lastI end ]
+            }
+
+            break
+        }
+
+        # chomp the next class/ID, and then continue
+        if { $lastI != 0 && $lastI < [expr $i-2] } {
+            if { $isClass } {
+                append class [ string range $tag $lastI [expr $i-2] ]
+            } else {
+                append id [ string range $tag $lastI [expr $i-2] ]
+            }
+        }
+
+        # info for the next iteration, to chomp from this to the next location
+        set isClass $nextClass
+        set lastI $i
     }
 
-    return "<$tag>$content</$tag>"
+    # finally build the tag it's self
+    set openTag "[ getTagType $tag ]"
+    if { $id != {} } {
+        append openTag " id=\"$id\""
+    }
+
+    if { $class != {} } {
+        append openTag " class=\"$class\""
+    }
+
+    return $openTag
 }
+
---
+++ src/html/html_test.tcl
@@ -0,0 +1,9 @@
+
+source {./html.tcl}
+
+puts {}
+html::html h1 blah blah
+html::html #blah.title blah blah
+html::html canvas#blah.title blah blah
+puts {}
+
